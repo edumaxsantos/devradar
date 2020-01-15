4e6f7f9f4b892e7c3e9fbbba071b4a135a8d8b5b
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2020-01-15 19:01:47 -0300

    INITIAL COMMIT

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+require('dotenv').config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_CONNECTION, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use(express.json());
+app.use(routes);
+
+
+// Query params: req.query (Filtros, ordenação, paginação, ...)
+// Route params: req.params (Identificar um recurso na alteração ou remoção)
+// Body: req.body (Dados para criação ou alteração de um registro)
+
+app.listen(3333);
