0b227350622b145eb5aa6dff900bf0c18db31fad
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2020-01-15 21:02:52 -0300

    Added CORS support

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
@@ -10,6 +11,7 @@ mongoose.connect(process.env.MONGO_CONNECTION, {
   useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
